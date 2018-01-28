0a0de4a146d44e63124532a92adc38b047a79458
Jasmine Quach <xjasminequach@gmail.com>
2018-01-27 18:29:28 -0600

    Got the fucking inventoryTypes finally

--- app/models/fabrics_inventory_type.rb
+++ app/models/fabrics_inventory_type.rb
@@ -1,4 +1,5 @@
 class FabricsInventoryType < ApplicationRecord
   belongs_to :fabric
-  has_one :inventory_type, :dependent => :destroy
+  # has_one :inventory_type, :dependent => :destroy
+  belongs_to :inventory_type, :dependent => :destroy
 end
