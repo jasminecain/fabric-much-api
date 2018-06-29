88e08e6567bd6c414945e02cef35a129b9021dff
Jasmine Quach <xjasminequach@gmail.com>
2018-06-28 23:47:11 -0500

    Revert "Testing a fix"
    This reverts commit 359ebeb3bf74113da951abfa19369c1a13bf0d34.

--- db/migrate/20171206165531_create_fabrics.rb
+++ db/migrate/20171206165531_create_fabrics.rb
@@ -3,6 +3,7 @@ class CreateFabrics < ActiveRecord::Migration[5.1]
     create_table :fabrics do |t|
       t.references :user, foreign_key: true, null: true
       t.references :bolt, foreign_key: true
+      t.references :fabric_type, foreign_key: true, null: true
       t.string :fabric_name
       t.integer :barcode
       t.float :price
