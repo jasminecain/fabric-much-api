359ebeb3bf74113da951abfa19369c1a13bf0d34
Jasmine Quach <xjasminequach@gmail.com>
2018-06-28 22:56:33 -0500

    Testing a fix

--- db/migrate/20171206165524_create_bolts.rb
+++ db/migrate/20171206165524_create_bolts.rb
@@ -2,7 +2,6 @@ class CreateBolts < ActiveRecord::Migration[5.1]
   def change
     create_table :bolts do |t|
       t.references :user, foreign_key: true
-      t.references :fabric_type, foreign_key: true
       t.string :fabric_name
       t.integer :barcode
       t.float :price
