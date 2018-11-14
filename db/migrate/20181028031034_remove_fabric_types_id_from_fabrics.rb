41560244deb50118d9478fa4616a2778e7e7c34c
Jasmine Quach <xjasminequach@gmail.com>
2018-11-13 19:01:38 -0600

    Missed one, deleteing one more migration

--- db/migrate/20181028031034_remove_fabric_types_id_from_fabrics.rb
+++
@@ -1,6 +0,0 @@
-class RemoveFabricTypesIdFromFabrics < ActiveRecord::Migration[5.1]
-  def change
-    remove_index :fabrics, :fabric_types_id
-    remove_column :fabrics, :fabric_types_id, :string
-  end
-end
